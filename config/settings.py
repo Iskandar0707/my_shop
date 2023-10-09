88ac2e250f1d867cb1214e457ab373f3d117e6dd
Iskandar <you@example.com>
2023-10-09 22:05:06 +0500

    urls,model,view,admin lar qoshildi

--- config/settings.py
+++ config/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uz-uz'
 
-TIME_ZONE = '+5'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
